c64b4e2339dfd6740966b9eed47db4727c7fc875
Rajendra <itsrajendrapatel@gmail.com>
2024-03-09 17:04:46 +0530

    Day-3 Done

---
+++ Day-3.py
@@ -0,0 +1,3 @@
+# On Day-3, we learn about Python buildIn module and external modules. I mean what is the use of them and how can we install them.
+
+# We also get to know about Pip, which is a python package manager and it very easy to use...
